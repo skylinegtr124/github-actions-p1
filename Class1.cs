695c416515de1c8a893e835a04dcfe3662b24423
skylinegtr124 <80573605+skylinegtr124@users.noreply.github.com>
2024-02-24 21:54:19 +0300

    Add files via upload
    Add project (web server)

---
+++ Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server
+{
+
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener serverListenter;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            this.serverListenter = new TcpListener(this.ipAddress, port);
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"Server started on port {port}.");
+            Console.WriteLine("Listening for requests...");
+
+            while (true)
+            {
+                this.serverListenter.Start();
+
+                var connection = serverListenter.AcceptTcpClient();
+
+                var networkStream = connection.GetStream();
+
+                WriteResponse(networkStream, "Hello there!");
+
+                connection.Close();
+            }
+        }
+
+        private void WriteResponse(NetworkStream networkStream, string message)
+        {
+            var contentLength = Encoding.UTF8.GetByteCount(message);
+
+            var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{message}";
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+
+            networkStream.Write(responseBytes, 0, responseBytes.Length);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ipAddress = "127.0.0.1"; // IP-адрес, на котором будет запущен сервер
+            int port = 8081; // Порт, на котором будет слушать сервер
+
+            BasicWebServer.Server.HttpServer server = new BasicWebServer.Server.HttpServer(ipAddress, port);
+            server.Start();
+
+            Console.ReadLine(); // Добавляем для того, чтобы консоль не закрывалась сразу после запуска сервера
+        }
+    }
+}
